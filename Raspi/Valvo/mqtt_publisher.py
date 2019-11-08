af9baa317f8548411ed0c38421c21a9cbb1312fb
alcalinebattery <jaska.98@windowslive.com>
2019-11-08 10:34:41 +0200

    Move scripts to Valvo folder, add cameratest script, modify script to create a log folder if not present.

--- Raspi/Valvo/mqtt_publisher.py
+++ Raspi/Valvo/mqtt_publisher.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 import paho.mqtt.client as mqtt
 import datetime
 dt = datetime.datetime.now()
 # Add reading sensor data directly from arduino modules, then save them to a file AND send them forward!
 
-# Open a new sensor "log file" every time we open this program:
+# Open a new sensor "log file" every time we open this program.
+# If the logs directory doesn't exist in the run directory, create one.
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
 filename = "valvo-log-" + dt.strftime('%Y-%m-%d-%H:%M:%S')
-logfile= open("/home/pi/Valvo/logs/" + filename, "w")
+logfile= open("logs/" + filename, "w")
+logfile.writelines("Valvo mqtt publisher log started.")
 msg="PLACEHOLDER"
 topic = "raspberry/sensor"
 
