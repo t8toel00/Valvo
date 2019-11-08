62ecf683f88efeb78f8846b6869aaf3db0ed34a7
alcalinebattery <jaska.98@windowslive.com>
2019-11-08 09:02:30 +0200

    Revert "Created Readme"
    This reverts commit a245f3639a1d37e33fab694732c7cb164b99b4e5.

--- Raspi/README.txt
+++
@@ -1,5 +0,0 @@
-Here are the needed files for functionality on the Raspberry end.
-
-You will need a Python3 environment with all the required modules.
-
-You will need to install v4l2 for the camera to work.
---
+++ Raspi/script_hello_Git_testikoodi.py
@@ -0,0 +1,22 @@
+#testikoodi Gitin kokeiluun, saa tehdä mitä vaan
+x = 3
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+else:
+    print("ei ollut yksi")
+# Muutokset tehty: x on nyt yksi ooo
+x = x - 1
+if x == 1:
+    # indented four spaces
+    print("x on 1.")
+else:
+    print("ei ollut vielakaan yksi")
+    
+#muokkaan tähän nämä rivit
+#Tähän lisää.
+#hello
+#hellotoinen
+#hellokolmas
+# TESTI 1
+# TeSTI 2 VS
