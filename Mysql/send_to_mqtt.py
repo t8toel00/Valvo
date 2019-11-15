f061a9c2d847f5bc588c9b0f8abfe93ae771c790
EliasT <t8toel00@students.oamk.fi>
2019-11-15 10:28:56 +0200

    Delete send_to_mqtt.py

--- Mysql/send_to_mqtt.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Test script to send mqtt messages.
-
-from mqtt_publisher import publishToMqtt
-
-top = input("Insert topic you want to send messages to:")
-
-publishToMqtt(top)
