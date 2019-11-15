4d1b6cd0b3bd7ee7fda8d282de747bca6217c9cc
alcalinebattery <jaska.98@windowslive.com>
2019-11-15 14:15:22 +0200

    Logging is now done from main.py

--- Raspi/Valvo/mqtt_publisher.py
+++ Raspi/Valvo/mqtt_publisher.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
 
-import os
-
 import paho.mqtt.client as mqtt
-import datetime
 
 # TO DO: Make this into a class object with class methods.
 # TO DO: Remove logfile from here and switch it into main.py
 class mqtt_conn():
 
 
-    def __init__(self):
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        self.dt = datetime.datetime.now()
-        self.filename = "valvo-log-" + self.dt.strftime('%Y-%m-%d-%H%M%S')
-        self.logfile= open("logs/" + self.filename, "w")
-        self.logfile.writelines("Valvo mqtt publisher log started.")
+    #def __init__(self):
 
     def connectMqtt(self, addr, port):
         # Open a new sensor "log file" every time we open this program.
@@ -28,10 +19,8 @@ class mqtt_conn():
         
         #while msg != "exit":
             #msg=input("Insert a message to send to the topic '" + topic + "': " + chr(10))
-        self.logfile.writelines(msg + "\n")
         self.client.publish(topic, msg)
 
     # Close the log file and disconnect from the broker:
     def closeMqtt():
-        self.logfile.close
         self.client.disconnect();
