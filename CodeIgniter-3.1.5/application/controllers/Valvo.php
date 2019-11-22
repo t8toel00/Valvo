b7135dee8b4ed24c56f438f64f23d93776e569da
EliasT <t8toel00@students.oamk.fi>
2019-11-22 15:03:50 +0200

    Cleaner website, navbar & subpages (Valvo.php)

--- CodeIgniter-3.1.5/application/controllers/Valvo.php
+++ CodeIgniter-3.1.5/application/controllers/Valvo.php
@@ -5,11 +5,62 @@ class Valvo extends CI_Controller {
 
         public function index()
         {
+?>
+
+<head>
+  <title>Valvontadata</title>
+  <h1>Valvontadata</h1>
+</head>
+<br>
+
+<div class="topnav">
+  <a href="http://172.20.240.54/">All</a>
+  <a href="http://172.20.240.54/index.php/valvo/tables">Full database</a>
+  <a href="http://172.20.240.54/index.php/valvo/charts">Charts</a>
+  <a href="http://172.20.240.54/index.php/valvo/pictures">Pictures</a>
+</div>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+
+<p>Latest image: </p>
+ <img src="http://172.20.240.54/images/lastshot.jpg" width="960" height="540" />
+ <br>
+
+<?php
+
             $this->load->helper('url');
 
             # Lataa codeigniterin DB-kirjaston, login asetukset ovat \application\config\database.php
             $this->load->database();
-                echo 'Valvontadata';
+                echo ' ';
                 echo '</br>';
                 echo '<a href="http://172.20.240.54/index.php/valvo/latest">Latest data</a>';
                 echo '</br>';
@@ -42,18 +93,37 @@ class Valvo extends CI_Controller {
          $query = $this->db->query('SELECT * FROM Arduino2 ORDER BY a2_aika DESC');
          echo $this->table->generate($query);
          echo '</br>';
-        
-         
-         # Ladataan kirjastosta hakemistoauttaja ja ladataan kuvia sivulle
-        $this->load->helper('url');
+
+# Table CSS styling       
 ?>
-        <img src="<?php echo base_url('images/tables_01.jpg'); ?>" /> 
+<style>
+{
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
+td, th {
+  border: 1px solid #ddd;
+  padding: 6px;
+}
 
+tr:nth-child(even){background-color: #f2f2f2;}
+
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 3px;
+  padding-bottom: 3px;
+  text-align: left;
+  background-color: #333;
+  color: white;
+}
+</style>
 
         <?php
 # ------------------------------------------------------------------------------------------- 
-# LUO ERILLINEN TAULUKKO JA TRIGGAA DATA VAIKKA TUNTEIHIN; YNNÄÄ HAVAINNOT NIIHIN
+# LUO ERILLINEN DB TAULUKKO JA TRIGGAA DATA VAIKKA TUNTEIHIN; YNNÄÄ HAVAINNOT NIIHIN
 
  $dataPoints1 = array(
          array("label"=> "2012019-11-12 16:05:11.1120", "y"=> 2),
@@ -98,14 +168,14 @@ class Valvo extends CI_Controller {
                  itemclick: toggleDataSeries
          },
          data: [{
-                 type: "column",
+                 type: "area",
                  name: "to North",
                  indexLabel: "{y}",
                  yValueFormatString: "#0.##",
                  showInLegend: true,
                  dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
          },{
-                 type: "column",
+                 type: "area",
                  name: "to South",
                  indexLabel: "{y}",
                  yValueFormatString: "#0.##",
@@ -135,15 +205,176 @@ class Valvo extends CI_Controller {
  </html>                           
 
 
+
 <?php
         }
-# Alihakemisto
+
+########################################################################################
+########################################################################################
+########################################################################################
+
+# Alihakemistot
 # http://172.20.240.54/index.php/valvo/latest
 
-        public function latest()
+
+        public function tables()
         {
-                echo 'Look at this!';
-                $this->load->view('footer');
+?>       
+<head>
+  <title>Valvontadata</title>
+  <h1>Valvontadata</h1>
+</head>
+<br>
+
+<div class="topnav">
+  <a href="http://172.20.240.54/">All</a>
+  <a href="http://172.20.240.54/index.php/valvo/tables">Full database</a>
+  <a href="http://172.20.240.54/index.php/valvo/charts">Charts</a>
+  <a href="http://172.20.240.54/index.php/valvo/pictures">Pictures</a>
+</div>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+
+
+
+<?php
+        }
+#############################################################################################
+        public function charts()
+        {
+?>       
+<head>
+  <title>Valvontadata</title>
+  <h1>Valvontadata</h1>
+</head>
+<br>
+
+<div class="topnav">
+  <a href="http://172.20.240.54/">All</a>
+  <a href="http://172.20.240.54/index.php/valvo/tables">Full database</a>
+  <a href="http://172.20.240.54/index.php/valvo/charts">Charts</a>
+  <a href="http://172.20.240.54/index.php/valvo/pictures">Pictures</a>
+</div>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+
+
+
+<?php                
+
+
+        }
+###########################################################################################
+        public function pictures()
+        {
+?>       
+
+<head>
+  <title>Valvontadata</title>
+  <h1>Valvontadata</h1>
+</head>
+<br>
+
+<div class="topnav">
+  <a href="http://172.20.240.54/">All</a>
+  <a href="http://172.20.240.54/index.php/valvo/tables">Full database</a>
+  <a href="http://172.20.240.54/index.php/valvo/charts">Charts</a>
+  <a href="http://172.20.240.54/index.php/valvo/pictures">Pictures</a>
+</div>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+
+<p>Latest image: </p>
+ <img src="http://172.20.240.54/images/lastshot.jpg" width="960" height="540" />
+ <br>
+
+<?php                
+
+
         }
 }
 ?>
