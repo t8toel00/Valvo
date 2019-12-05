73abdc42c5e36ad8928c38fbc0503dd0c9a03d09
EliasT <t8toel00@students.oamk.fi>
2019-12-05 10:34:39 +0200

    a working "take a photo"-button in webpage

--- CodeIgniter-3.1.5/images/hellopython.py
+++ CodeIgniter-3.1.5/images/hellopython.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-print("toimiiko1")
+print("importing client")
+
+import sys
+print(sys.path)
+
+import paho.mqtt.client as mqtt
+
+print("Sending request ")
+client = mqtt.Client()
+client.connect("127.0.0.1",1883,60)
+client.publish("server/takephoto", "1")
+print("Request sent ")
+client.disconnect()
